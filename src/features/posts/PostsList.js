a74210110fc2fd544f699f93add45f9a53d4feb1
vanessuniq <vanessuniq@gmail.com>
2020-10-26 19:15:32 -0400

    display loading state or error state in UI

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -10,6 +10,7 @@ const PostsList = () => {
     const dispatch = useDispatch()
     const posts = useSelector(selectAllPosts)
     const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
 
     useEffect(() => {
         if(postStatus === 'idle') {
@@ -17,25 +18,34 @@ const PostsList = () => {
         }
         
     }, [postStatus, dispatch]);
-    const sortedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-    const renderedPosts = sortedPosts.map(post => ( 
-        <article className = "post-excerpt" key = { post.id } >
-            <h3 > { post.title } </h3> 
-            <PostAuthor userId={post.userId}/>
-            <TimeAgo timestamp={post.date}/>
-            <br/><br/>
-            <p > { post.content.substring(0, 100) } </p>
-            <ReactionButtons post={post}/>
-            <Link to = { `/posts/${post.id}` } className = 'button muted-button'>
-                View Post 
-            </Link> 
-        </article>
-    ))
+
+    let content;
+
+    if (postStatus === 'loading') {
+        content = <div className="loader">Loading...</div>
+    } else if(postStatus === 'succeeded') {
+        const sortedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = sortedPosts.map(post => ( 
+            <article className = "post-excerpt" key = { post.id } >
+                <h3 > { post.title } </h3> 
+                <PostAuthor userId={post.userId}/>
+                <TimeAgo timestamp={post.date}/>
+                <br/><br/>
+                <p > { post.content.substring(0, 100) } </p>
+                <ReactionButtons post={post}/>
+                <Link to = { `/posts/${post.id}` } className = 'button muted-button'>
+                    View Post 
+                </Link> 
+            </article>
+        ))
+    } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
+    }
 
     return ( 
         <section>
             <h2 > Posts </h2> 
-            { renderedPosts } 
+            { content } 
         </section>
     );
 }
